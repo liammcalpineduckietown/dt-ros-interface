41644824776d3f025f2221cf44ad1faa02172a57
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-17 23:59:09 -0400

    DTSW-5235 - Implemented ROS interface to Time-of-Flight sensor

---
+++ launchers/sensor-tof-bottom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /environment.sh
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+exec roslaunch --wait tof_driver tof_driver_node.launch veh:=$VEHICLE_NAME name:=bottom
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
